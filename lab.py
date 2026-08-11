835826e932eb71367683f498679e1f320e26d1d6
mirandairene777 <mirandairene777@gmail.com>
2026-08-11 13:13:28 +0800

    Upload lab files

---
+++ lab.py
@@ -0,0 +1,11 @@
+name = "Irene Miranda"
+course = "BSCS"
+section = "BSCS3B"
+favorite_ai_tech = "claude"
+future_ai_project = "A simple AI study planner that helps students organize tasks and reminders"
+
+print("Name:", name)
+print("Course:", course)
+print("Section:", section)
+print("Favorite AI tech:", favorite_ai_tech)
+print("Future AI project:", future_ai_project)
